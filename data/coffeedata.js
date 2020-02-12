2f021865fb51d666d7873cdba8ccfab27d2c5ef2
hoferb <33937796+hoferb@users.noreply.github.com>
2020-02-12 20:29:01 +0100

    Add files via upload

---
+++ data/coffeedata.js
@@ -0,0 +1,762 @@
+coffee = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-02-10T19:04:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334455829",
+        "amenity": "cafe",
+        "description": "Kohvik grill",
+        "name": "Lõvisüdame",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7229377,
+          58.3804291
+        ]
+      },
+      "id": "node/334455829"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334456332",
+        "addr:city": "Tartu",
+        "amenity": "cafe",
+        "name": "Suudlevad tudengid",
+        "opening_hours": "Su-Mo 11:00-24:00; Tu-Th 11:00-01:00; Sa 11:00-07:00",
+        "outdoor_seating": "yes",
+        "phone": "+7301893",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7231282,
+          58.3804285
+        ]
+      },
+      "id": "node/334456332"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334458370",
+        "amenity": "cafe",
+        "cuisine": "crepe",
+        "internet_access": "wlan",
+        "name": "Crepp",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.721264,
+          58.3819095
+        ]
+      },
+      "id": "node/334458370"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334463255",
+        "amenity": "cafe",
+        "name": "Vaikne Nurgake",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7235127,
+          58.3816485
+        ]
+      },
+      "id": "node/334463255"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334466767",
+        "amenity": "cafe",
+        "name": "Illegaard",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7224081,
+          58.3793502
+        ]
+      },
+      "id": "node/334466767"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334471698",
+        "amenity": "cafe",
+        "name": "Underground",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7227145,
+          58.380798
+        ]
+      },
+      "id": "node/334471698"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/334471850",
+        "amenity": "cafe",
+        "name": "Rotund",
+        "name:ru": "Ротонда",
+        "outdoor_seating": "yes",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.716543,
+          58.380024
+        ]
+      },
+      "id": "node/334471850"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/448280796",
+        "amenity": "cafe",
+        "name": "Armastus Cafe Amore",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7222953,
+          58.3806885
+        ]
+      },
+      "id": "node/448280796"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/460016344",
+        "amenity": "cafe",
+        "name": "Tulbi kohvik",
+        "website": "http://www.tulbikohvik.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6908361,
+          58.3735417
+        ]
+      },
+      "id": "node/460016344"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/576250007",
+        "amenity": "cafe",
+        "level": "0",
+        "name": "Free Caffee",
+        "opening_hours": "Mo-Su 09:00-21:00",
+        "phone": "+372 7303850",
+        "website": "https://astri.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6779775,
+          58.3581642
+        ]
+      },
+      "id": "node/576250007"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/576250015",
+        "amenity": "cafe",
+        "level": "0",
+        "name": "Orient Express",
+        "opening_hours": "Mo-Su 10:00-21:00",
+        "phone": "+372 7315598",
+        "website": "https://astri.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6756803,
+          58.3585661
+        ]
+      },
+      "id": "node/576250015"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/576250034",
+        "amenity": "cafe",
+        "name": "Teaduspargi kohvik"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6798506,
+          58.3561505
+        ]
+      },
+      "id": "node/576250034"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1191320038",
+        "addr:city": "Tartu",
+        "addr:housenumber": "7",
+        "addr:street": "Küüni",
+        "amenity": "cafe",
+        "name": "Pagaripoisid",
+        "opening_hours": "Mo-Fr 09:00-20:30; Sa 09:00-18:00; Su 10:00-17:00",
+        "phone": "+372 56215066",
+        "website": "https://www.pagaripoisid.ee/",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.725182,
+          58.378094
+        ]
+      },
+      "id": "node/1191320038"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1273032558",
+        "amenity": "cafe",
+        "name": "Anna Edasi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7276515,
+          58.370312
+        ]
+      },
+      "id": "node/1273032558"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1383368338",
+        "amenity": "cafe",
+        "name": "Iuridikumi kohvik",
+        "opening_hours": "Mo-Fr 10:00-17:00",
+        "operator": "Du Nord",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.705594,
+          58.378252
+        ]
+      },
+      "id": "node/1383368338"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1632497825",
+        "amenity": "cafe",
+        "cuisine": "international",
+        "name": "Romaan",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7267332,
+          58.377811
+        ]
+      },
+      "id": "node/1632497825"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1890582115",
+        "amenity": "cafe",
+        "level": "0",
+        "name": "Coffee In",
+        "opening_hours": "Mo-Su 09:00-21:00",
+        "phone": "+372 6654811",
+        "website": "http://www.coffeein.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6769779,
+          58.35854
+        ]
+      },
+      "id": "node/1890582115"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1890582120",
+        "amenity": "cafe",
+        "level": "0",
+        "name": "Le Bus",
+        "opening_hours": "Mo-Su 10:00-21:00",
+        "phone": "+372 7315643"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6756199,
+          58.3581957
+        ]
+      },
+      "id": "node/1890582120"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1890582156",
+        "amenity": "cafe",
+        "level": "0",
+        "name": "Sõõrikumeistrid",
+        "opening_hours": "Mo-Su 10:00-21:00",
+        "phone": "+372 5029950"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6775837,
+          58.3583175
+        ]
+      },
+      "id": "node/1890582156"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1890582157",
+        "amenity": "cafe",
+        "cuisine": "sushi",
+        "level": "0",
+        "name": "Tokyo Sushibaar",
+        "opening_hours": "Mo-Su 10:00-21:00",
+        "phone": "+372 6543370",
+        "website": "http://www.tokyosushibaar.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6761096,
+          58.358818
+        ]
+      },
+      "id": "node/1890582157"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1946225209",
+        "addr:city": "Tartu",
+        "addr:country": "EE",
+        "addr:housenumber": "1",
+        "addr:postcode": "50411",
+        "addr:street": "Nooruse",
+        "amenity": "cafe",
+        "opening_hours": "Mo-Fr 10:00-17:00",
+        "operator": "DuNord",
+        "website": "http://www.dunord.ee/est/?p=48",
+        "wheelchair": "limited"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6943878,
+          58.3660955
+        ]
+      },
+      "id": "node/1946225209"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2408301530",
+        "amenity": "cafe",
+        "opening_hours": "Mo-Fr 09:00-16:00",
+        "operator": "Tartu Erakool",
+        "smoking": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7262113,
+          58.3735524
+        ]
+      },
+      "id": "node/2408301530"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2424663997",
+        "amenity": "cafe",
+        "name": "Katarina"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7288513,
+          58.3820129
+        ]
+      },
+      "id": "node/2424663997"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2957275969",
+        "amenity": "cafe",
+        "operator": "DuNord",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6934877,
+          58.3668319
+        ]
+      },
+      "id": "node/2957275969"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2957276000",
+        "amenity": "cafe",
+        "operator": "DuNord",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.6942395,
+          58.3682403
+        ]
+      },
+      "id": "node/2957276000"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3684465329",
+        "addr:city": "Tartu",
+        "addr:housenumber": "27b",
+        "addr:postcode": "51013",
+        "addr:street": "Narva mnt",
+        "amenity": "cafe",
+        "name": "Tüse Tort & Krõnksus Kringel"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7276555,
+          58.3834847
+        ]
+      },
+      "id": "node/3684465329"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3755395756",
+        "amenity": "cafe",
+        "name": "Kohvik Ave",
+        "opening_hours": "Mo-Fr 10:00-15:00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7272727,
+          58.3500486
+        ]
+      },
+      "id": "node/3755395756"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4247172293",
+        "addr:city": "Tartu",
+        "addr:country": "EE",
+        "addr:housenumber": "5A",
+        "addr:postcode": "51005",
+        "addr:street": "Veski",
+        "amenity": "cafe",
+        "name": "Mandel",
+        "opening_hours": "Tu-Fr 07:30-11:00,16:00-19:00; Sa 09:00-15:00",
+        "takeaway": "yes",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7094408,
+          58.3821238
+        ]
+      },
+      "id": "node/4247172293"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4260173665",
+        "amenity": "cafe",
+        "description": "Kondiiter",
+        "name": "Pihlaka",
+        "opening_hours": "Mo-Sa 09:00-20:00, Su 09:00-19:00",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7251061,
+          58.3799968
+        ]
+      },
+      "id": "node/4260173665"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4293444689",
+        "amenity": "cafe",
+        "email": "info@shogun.ee",
+        "internet_access": "wlan",
+        "name": "Cafe Shogun",
+        "opening_hours": "Sa-Su 11:00-22:00; We-Fr 10:00-22:00; Mo-Tu 10:00-22:00",
+        "website": "http://www.shogun.ee",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7210795,
+          58.3681603
+        ]
+      },
+      "id": "node/4293444689"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4414529090",
+        "amenity": "cafe",
+        "name": "Kohvipaus",
+        "name:et": "Kohvipaus",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7218146,
+          58.3810718
+        ]
+      },
+      "id": "node/4414529090"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4527987089",
+        "amenity": "cafe",
+        "name": "Karlova Kohvik",
+        "name:en": "Karlova Kohvik",
+        "name:et": "Karlova Kohvik",
+        "name:fr": "Karlova Kohvik"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7295615,
+          58.366675
+        ]
+      },
+      "id": "node/4527987089"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4642071492",
+        "amenity": "cafe",
+        "cuisine": "regional",
+        "name": "Krempel",
+        "name:et": "Krempel",
+        "opening_hours": "Mo-Fr 08:00-22:00; Sa 09:00-22:00; Su 09:00-18:00",
+        "phone": "+372 445 1510",
+        "website": "http://www.krempel.ee/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.721469,
+          58.381526
+        ]
+      },
+      "id": "node/4642071492"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4813033694",
+        "addr:city": "Tartu",
+        "addr:housenumber": "42",
+        "addr:postcode": "50410",
+        "addr:street": "Kastani",
+        "amenity": "cafe",
+        "breakfast": "yes",
+        "contact:email": "triin@trikstertihane.ee",
+        "contact:facebook": "@trikstertihane",
+        "dinner": "yes",
+        "lunch": "yes",
+        "name": "Trikster Tihane",
+        "opening_hours": "10:00-22:00",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7158006,
+          58.3709945
+        ]
+      },
+      "id": "node/4813033694"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4912160021",
+        "amenity": "cafe",
+        "name": "Rajaaare Kook",
+        "name:et": "Rajaaare Kook"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7668919,
+          58.4185832
+        ]
+      },
+      "id": "node/4912160021"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5109159139",
+        "amenity": "cafe",
+        "name": "Jop"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7185448,
+          58.3821421
+        ]
+      },
+      "id": "node/5109159139"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5584307359",
+        "amenity": "cafe",
+        "name": "DuNord"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7240491,
+          58.3868356
+        ]
+      },
+      "id": "node/5584307359"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/6466409285",
+        "amenity": "cafe",
+        "cuisine": "asian",
+        "name": "Ginger Grill",
+        "phone": "+37256683044"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7240596,
+          58.3786003
+        ]
+      },
+      "id": "node/6466409285"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/6466419986",
+        "addr:housenumber": "28a",
+        "addr:street": "Kalda tee",
+        "amenity": "cafe",
+        "cuisine": "chinese;asian",
+        "name": "China Box",
+        "phone": "+3723850075"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.7653891,
+          58.3716474
+        ]
+      },
+      "id": "node/6466419986"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/6577678985",
+        "amenity": "cafe",
+        "name": "Säde"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.722043,
+          58.3807033
+        ]
+      },
+      "id": "node/6577678985"
+    }
+  ]
+}
